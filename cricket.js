0191a014a3acf5aff8fef6168386fc6225e4d414
Harshawardhan More <harshaiitdhanbad@gmail.com>
2025-10-16 23:14:53 +0530

    Refactor cricket game score management and display
    added local storage and reset button

--- cricket.js
+++ cricket.js
@@ -1,45 +1,56 @@
+let scoreStr = localStorage.getItem("score");
+let score = JSON.parse(scoreStr) || {
+  win: 0,
+  tie: 0,
+  lost: 0,
+};
 
-        let score={
-        win:0,
-        tie:0,
-        lost:0,
-        displayScore:function () {
-          return  `won:${score.win} lost:${score.lost} tie:${score.tie}`
-          
-        }
+function displayScore() {
+  return `won:${score.win} lost:${score.lost} tie:${score.tie}`;
+}
+function resetScore(scoreStr) {
+  score = scoreStr
+    ? JSON.parse(scoreStr)
+    : {
+        win: 0,
+        tie: 0,
+        lost: 0,
       };
-    function playGame(userChoice) {
-      // declare local variables
-      const randomNumber = Math.random() * 3;
-      let computerChoice; // ← IMPORTANT: declare it!
+}
+function showResult(userChoice, computerChoice, resultMsg) {
+  localStorage.setItem("score", JSON.stringify(score));
+  alert(`You chose ${userChoice}\nComputer chose ${computerChoice}\n${resultMsg}
+       ${displayScore()} `);
+  console.log(score);
+}
+function playGame(userChoice) {
+ 
+  const randomNumber = Math.random() * 3;
+  let computerChoice; 
+
+  if (randomNumber <= 1) {
+    computerChoice = "Bat";
+  } else if (randomNumber <= 2) {
+    computerChoice = "Ball";
+  } else {
+    computerChoice = "Stumps";
+  }
 
 
-      if (randomNumber <= 1) {
-        computerChoice = 'Bat';
-      } else if (randomNumber <= 2) {
-        computerChoice = 'Ball';
-      } else {
-        computerChoice = 'Stumps';
-      }
-
-      // determine result
-      let resultMsg;
-      if (userChoice === computerChoice) {
-        resultMsg = "It's a tie!";
-        score.tie++;
-      } else if (
-        (userChoice === 'Bat' && computerChoice === 'Ball') ||
-        (userChoice === 'Ball' && computerChoice === 'Stumps') ||
-        (userChoice === 'Stumps' && computerChoice === 'Bat')
-      ) {
-        resultMsg = 'You won!';
-        score.win++;
-      } else {
-        resultMsg = 'Computer won!';
-        score.lost++;
-      }
-
-      alert(`You chose ${userChoice}\nComputer chose ${computerChoice}\n${resultMsg}
-       ${score.displayScore()} `);
-      console.log(score);
-    }
+  let resultMsg;
+  if (userChoice === computerChoice) {
+    resultMsg = "It's a tie!";
+    score.tie++;
+  } else if (
+    (userChoice === "Bat" && computerChoice === "Ball") ||
+    (userChoice === "Ball" && computerChoice === "Stumps") ||
+    (userChoice === "Stumps" && computerChoice === "Bat")
+  ) {
+    resultMsg = "You won!";
+    score.win++;
+  } else {
+    resultMsg = "Computer won!";
+    score.lost++;
+  }
+  showResult(userChoice, computerChoice, resultMsg);
+}
