39eb749b2136059b5f50539faec5923dabdaffe0
Harshawardhan More <harshaiitdhanbad@gmail.com>
2025-10-16 20:33:22 +0530

    Implement score tracking in playGame function
    Added scoring system to track wins, losses, and ties.

--- cricket.js
+++ cricket.js
@@ -1,8 +1,18 @@
 
+        let score={
+        win:0,
+        tie:0,
+        lost:0,
+        displayScore:function () {
+          return  `won:${score.win} lost:${score.lost} tie:${score.tie}`
+          
+        }
+      };
     function playGame(userChoice) {
       // declare local variables
       const randomNumber = Math.random() * 3;
       let computerChoice; // ← IMPORTANT: declare it!
+
 
       if (randomNumber <= 1) {
         computerChoice = 'Bat';
@@ -16,15 +26,20 @@
       let resultMsg;
       if (userChoice === computerChoice) {
         resultMsg = "It's a tie!";
+        score.tie++;
       } else if (
         (userChoice === 'Bat' && computerChoice === 'Ball') ||
         (userChoice === 'Ball' && computerChoice === 'Stumps') ||
         (userChoice === 'Stumps' && computerChoice === 'Bat')
       ) {
         resultMsg = 'You won!';
+        score.win++;
       } else {
         resultMsg = 'Computer won!';
+        score.lost++;
       }
 
-      alert(`You chose ${userChoice}\nComputer chose ${computerChoice}\n${resultMsg}`);
+      alert(`You chose ${userChoice}\nComputer chose ${computerChoice}\n${resultMsg}
+       ${score.displayScore()} `);
+      console.log(score);
     }
